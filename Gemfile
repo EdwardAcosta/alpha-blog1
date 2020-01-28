4165d3db21680d08db267a397f2a72bfcccbd324
Edward Rene Acosta <edwardacosta@Edwards-Air.satx.rr.com>
2020-01-28 12:09:05 -0600

    Reset database due to errors

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ ruby '2.6.3'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 # Use sqlite3 as the database for Active Record
 
+# gem sassc
+#gem 'sassc'
+
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_10_013222) do
+ActiveRecord::Schema.define(version: 2020_01_10_030239) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
